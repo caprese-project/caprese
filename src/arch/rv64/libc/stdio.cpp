5accd9a17321256edfa6a02073c6eaa349bd3ef7
cosocaf <cosocaf@gmail.com>
2023-12-27 02:37:11 +0900

    file

--- src/arch/rv64/libc/stdio.cpp
+++ src/arch/rv64/libc/stdio.cpp
@@ -1,30 +1,45 @@
+#include <cstdio>
+
 #include <kernel/arch/sbi.h>
 
-extern "C" {
-  struct __FILE {
-    void write(int ch) {
-      sbi_console_putchar(ch);
+namespace {
+  size_t stdio_read(void*, size_t, size_t, FILE*) {
+    return 0;
+  }
+
+  size_t stdio_write(const void* __restrict ptr, size_t size, size_t nmemb, FILE*) {
+    for (size_t i = 0; i < size * nmemb; ++i) {
+      sbi_console_putchar(static_cast<const char*>(ptr)[i]);
     }
+    return size * nmemb;
+  }
+
+  int stdio_ungetc(int, FILE*) {
+    return EOF;
+  }
+
+  FILE stdio_file = {
+    .__buf_mode   = _IONBF,
+    .__ungetc_buf = static_cast<char>(EOF),
+    .__buf        = nullptr,
+    .__buf_size   = 0,
+    .__buf_pos    = 0,
+    .__read       = stdio_read,
+    .__write      = stdio_write,
+    .__ungetc     = stdio_ungetc,
   };
+} // namespace
 
-  namespace {
-    struct __FILE __stdout_file;
-  } // namespace
-
-  struct __FILE* __stdin() {
-    return &__stdout_file;
+extern "C" {
+  FILE* __stdin() {
+    return &stdio_file;
   }
 
-  struct __FILE* __stdout() {
-    return &__stdout_file;
+  FILE* __stdout() {
+    return &stdio_file;
   }
 
-  struct __FILE* __stderr() {
-    return &__stdout_file;
-  }
-
-  int fputc(int ch, struct __FILE* stream) {
-    stream->write(ch);
-    return ch;
+  FILE* __stderr() {
+    return &stdio_file;
   }
 }
